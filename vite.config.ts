7e9fea603693c23a96c60485aca12fb25b7caad6
gersonbrenof <gersonfagundese2016@gmail.com>
2025-07-25 11:26:53 -0300

    adcionado aquivos no vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // atalho opcional
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+  },
+  // necessário para deploy de SPA com react-router
+  base: './',
 })
